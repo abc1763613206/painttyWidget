e3cb28733ce53ce6e6b7e67f7b71de400cb10154
liuyanghejerry <liuyanghejerry@126.com>
2025-02-06 18:06:41 +0800

    feat: 增加macOS的自动构建脚本

---
+++ scripts/build-for-mac.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 如果QTDIR变量未设置，则报错终止
+if [ -z "$QTDIR" ]; then
+    echo "QTDIR变量未设置，请设置QTDIR变量后运行此脚本"
+    exit 1
+fi
+
+export PATH="$QTDIR/bin:$PATH"
+
+# 获取脚本所在目录的绝对路径
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+BUILD_DIR=$(cd "$SCRIPT_DIR/../build" && pwd)
+PROJECT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
+
+echo "PROJECT_DIR: $PROJECT_DIR"
+echo "BUILD_DIR: $BUILD_DIR"
+
+QMAKE_PATH="$QTDIR/clang_64/bin/qmake"
+DEPLOY_PATH="$QTDIR/clang_64/bin/macdeployqt"
+
+# 先执行qmake
+$QMAKE_PATH "$PROJECT_DIR/painttyWidget.pro" -spec macx-clang CONFIG+=qtquickcompiler && /usr/bin/make qmake_all
+
+# 再执行make
+cd "$BUILD_DIR/main-Release" && /usr/bin/make -j10
+cd "$BUILD_DIR/main_intel-Release" && /usr/bin/make -j10
+
+# 再执行macdeployqt
+# $DEPLOY_PATH "$BUILD_DIR/main-Release/build/MrPaint.app"
+# $DEPLOY_PATH "$BUILD_DIR/main_intel-Release/build/MrPaint.app"
